f143922f8159d057225d5f077fd9e4b080e1ce15
Анна Красновид <88835714+AnnaKrasnovid@users.noreply.github.com>
2023-08-10 15:18:32 +0300

    steps, save image

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import './index.css';
+import 'normalize.css';
+
+import { hexToRgb } from './utils/functions';
 
 const drawingSheet = document.querySelector('.image');
 const buttonClear = document.querySelector('.button-clear');
+const buttonRedo = document.querySelector('.button_type_redo');
+const buttonUndo = document.querySelector('.button_type_undo');
+const buttonSave = document.querySelector('.button_type_save');
 const inputColor = document.querySelector('.input-color');
 const inputOpasity = document.querySelector('.input-range_type_opasity');
 const inputWeight = document.querySelector('.input-range_type_weight');
-const buttonRedo = document.querySelector('.button_type_redo');
-const buttonUndo = document.querySelector('.button_type_undo');
+const textColor = document.querySelector('.input-box__option_type_color');
+const textOpasity = document.querySelector('.input-box__option_type_opasity');
+const textWeight = document.querySelector('.input-box__option_type_weight');
 const canvas = document.querySelector('.canvas');
 let context = canvas.getContext('2d');
 
@@ -23,6 +30,10 @@ let opasityLine = 100
 let colorLine = '#000000';
 let colorRGBA = `rgba(0,0,0, ${opasityLine})`
 let step = 0;
+
+textColor.textContent = colorLine
+textOpasity.textContent = opasityLine + '%'
+textWeight.textContent = weightLine + 'px'
 
 const heightCanvas = () => drawingSheet.offsetHeight;
 const widthCanvas = () => drawingSheet.offsetWidth;
@@ -50,19 +61,17 @@ function finishedPosition() {
     painting = false
     lines.push(line)
     line = []
+    step = lines.length
 }
 
-function drawLine(e) {
-    mousePositionX = e.offsetX;
-    mousePositionY = e.offsetY;
-
-    context.lineWidth = weightLine
+function drawLine(x, y, weight, color) {
+    context.lineWidth = weight
     context.lineCap = 'round'
-    context.strokeStyle = colorRGBA
-    context.lineTo(mousePositionX, mousePositionY)
+    context.strokeStyle = color
+    context.lineTo(x, y)
     context.stroke()
     context.beginPath();
-    context.moveTo(mousePositionX, mousePositionY)
+    context.moveTo(x, y)
     context.beginPath();
 }
 
@@ -71,8 +80,10 @@ function draw(e) {
     if (!painting) {
         return
     }
-
-    drawLine(e)
+    lines = lines.slice(0, step)
+    mousePositionX = e.offsetX;
+    mousePositionY = e.offsetY;
+    drawLine(mousePositionX, mousePositionY, weightLine, colorRGBA)
 
     line.push({ x: mousePositionX, y: mousePositionY, weight: weightLine, color: colorRGBA })
     // если при нажатой мыши курсов вышел за пределы canvas, то рисование сбрасываем
@@ -80,55 +91,76 @@ function draw(e) {
     if (mousePositionX + 10 > drawingSheet.offsetWidth || mousePositionX <= 0 || mousePositionY + 10 > drawingSheet.offsetHeight || mousePositionY < 10) {
         painting = false
     }
-    // console.log(line)
 }
 
 function clearCanvas() {
     context.clearRect(0, 0, widthCanvas(), heightCanvas());
 }
 
-export function hexToRgb(hex) {
-
-    let shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-
-    hex = hex.replace(shorthandRegex, function (r, g, b) {
-        return r + r + g + g + b + b;
-    });
-
-    let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-
-    return result ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16)
-    } : null;
+function startOver() {
+    context.clearRect(0, 0, widthCanvas(), heightCanvas());
+    lines = [];
+    line = []
 }
 
 function changeHexToRgba(color) {
     const colorConvert = hexToRgb(color)
-    return `rgba(${colorConvert?.r}, ${colorConvert?.g}, ${colorConvert?.b}, ${opasityLine / 100})`
+    colorRGBA = `rgba(${colorConvert?.r}, ${colorConvert?.g}, ${colorConvert?.b}, ${opasityLine / 100})`
 }
+
+textColor.textContent = colorLine
+textOpasity.textContent = opasityLine + '%'
+textWeight.textContent = weightLine + 'px'
 
 function changeColor(e) {
     colorLine = e.target.value
-    colorRGBA = changeHexToRgba(colorLine)   
+    changeHexToRgba(colorLine)
+    textColor.textContent = colorLine
 }
 
 function changeWeight(e) {
     weightLine = e.target.value
+    textWeight.textContent = weightLine + 'px'
 }
 
 function changeOpasity(e) {
     opasityLine = e.target.value
-    colorRGBA = changeHexToRgba(colorLine)
+    changeHexToRgba(colorLine)
+    textOpasity.textContent = opasityLine + '%'
+}
+
+function renderLine() {
+    clearCanvas()
+
+    lines.slice(0, step).forEach((item) => {
+        item.forEach(i => {
+            drawLine(i.x, i.y, i.weight, i.color)
+        })
+    })
 }
 
 function goBackStep() {
-
+    if (step === 0) {
+        return;
+    }
+    step = step - 1;
+    renderLine();
 }
 
 function goForwardStep() {
-    
+    if (step === lines.length) {
+        return;
+    }
+    step = step + 1;
+    renderLine();
+}
+function saveImage() {
+    let downloadLink = document.createElement('a');
+    downloadLink.setAttribute('download', 'Image.png');
+    let dataURL = canvas.toDataURL('image/png');
+    let url = dataURL.replace(/^data:image\/png/, 'data:application/octet-stream');
+    downloadLink.setAttribute('href', url);
+    downloadLink.click();
 }
 
 changeSizeCanvas()
@@ -136,12 +168,12 @@ window.addEventListener('resize', () => changeSizeCanvas())
 canvas.addEventListener('mousedown', startPosition)
 canvas.addEventListener('mouseup', finishedPosition)
 canvas.addEventListener('mousemove', (e) => handleChangeCanvas(e))
-buttonClear.addEventListener('click', clearCanvas)
+buttonClear.addEventListener('click', startOver)
 inputColor.addEventListener('change', (e) => changeColor(e))
 inputOpasity.addEventListener('change', (e) => changeOpasity(e))
 inputWeight.addEventListener('change', (e) => changeWeight(e))
-buttonRedo.addEventListener('click', goBackStep)
-buttonUndo.addEventListener('click', goForwardStep)
-
+buttonRedo.addEventListener('click', goForwardStep)
+buttonUndo.addEventListener('click', goBackStep)
+buttonSave.addEventListener('click', saveImage)
 
 
